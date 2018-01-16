dc929e5d884f8ce19b06ffa3fde3ca22740e6626
James Marcogliese <james.marcogliese@gmail.com>
2018-01-15 20:43:26 -0500

    Added review function and tweaked menus

--- bin/app.py
+++ bin/app.py
@@ -3,11 +3,15 @@
 A routing layer for the Hive Slack app 
 """
 from slackserver import SlackEventAdapter
+from elasticsearch import Elasticsearch
 from slackclient import SlackClient
 import os
 
 # Import commands module
 import commands
+
+# Connect to Elasticsearch instance
+commands.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 # Our app's Slack Event Adapter for receiving actions via the Events API
 SLACK_VERIFICATION_TOKEN = os.environ["SLACK_VERIFICATION_TOKEN"]
@@ -15,28 +19,17 @@ slack_events_adapter = SlackEventAdapter(SLACK_VERIFICATION_TOKEN, "/slack")
 
 # Create a SlackClient for your bot to use for Web API requests
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-slack_client = SlackClient(SLACK_BOT_TOKEN)
-
-# Flask webserver for incoming traffic from Slack
-#app = Flask(__name__)
+commands.slack_client = SlackClient(SLACK_BOT_TOKEN)
 
 # Responder to direct messages
 @slack_events_adapter.on("message")
 def handle_message(event_data):
-	message = event_data["event"]
-	
-	if message.get("subtype") is None: 
-		print (event_data)
-		channel = message["channel"]
-		message,attachment = commands.identify_command(event_data)
-		slack_client.api_call("chat.postMessage", channel=channel, text=message, attachments=attachment)
+	if event_data.get("event").get("subtype") is None: 
+		commands.command_invoker(event_data)
 		
 @slack_events_adapter.on("message_action")
 def message_action(action_data):
-	channel = action_data["channel"]["id"]
-	message,attachment = commands.identify_callback(action_data)
-	slack_client.api_call("chat.postMessage", channel=channel, text=message, attachments=attachment)
-	
+	commands.action_invoker(action_data)
 
 # Flask server with the default endpoint on port 3000
 slack_events_adapter.start(port=3000)
--- bin/commands.py
+++ bin/commands.py
@@ -2,19 +2,18 @@
 """
 A module for handling commands for the Hive Slack app 
 """
-from elasticsearch import Elasticsearch
 from datetime import datetime
 from pathlib import Path
 import requests
 import json
 import re
 
-
-# Connect to Elasticsearch instance
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+es = None
+slack_client = None
 
 def save_note(event_data):
 	print ("SaveNote")
+	channel = event_data["event"]["channel"]
 	
 	#Search for author's team from author index
 	query = {
@@ -28,21 +27,25 @@ def save_note(event_data):
 		author = es.search(index="author-index", body=query)
 	
 	if (author['hits']['total'] == 0) or not(es.indices.exists(index="author-index")):
-		return """Wait! :raised_hand:I need to know what team you're on before I can save your notes! 
-				  Please set your team by using the 'team' command!"""
+		message = ("Wait! :raised_hand:I need to know what team you're on before I can save your notes!\n" 
+				   "Please set your team by using the 'team' command!"
+				   )
+		slack_client.api_call("chat.postMessage", channel=channel, text=message, attachments=None)
+		return 
 	
 	# Save note to note-index
 	doc = {
-		'note': event_data['event']['text'].lstrip('save '),
-		'author': event_data['event'],
+		'note': event_data["event"]["text"].lstrip("save "),
+		'author': event_data["event"]["user"],
 		'frequency': 0,
-		'team': author['hits']['hits'][0]['_source']['team'],
+		'team': author["hits"]["hits"][0]["_source"]["team"],
 		'timestamp': datetime.now()
 		}
 	
-	es.index(index="note-index", doc_type='note', body=doc, refresh="true") 
+	es.index(index="note-index", doc_type="note", body=doc, refresh="true") 
 
-	return "Note saved! :notebook:", None
+	slack_client.api_call("chat.postMessage", channel=channel, text="Note saved! :notebook:", attachments=None)
+	return
 
 def find_note(event_data):
 	print ("findNote")
@@ -53,14 +56,15 @@ def set_team(event_data):
 	
 	if event_data["type"] == 'event_callback':
 		# Send the list to the user
-		menu = json.load(open('teams_menu.json'))
-		return "What team do you belong to?", menu
+		teams_menu = json.load(open('teams_menu.json'))
+		channel = event_data["event"]["channel"]
+		slack_client.api_call("chat.postMessage", channel=channel, text="What team do you belong to?", attachments=teams_menu)
+		return
 	elif event_data["type"] == 'interactive_message':
 		# Check the users selection
 		user = event_data["user"]["id"]
 		selected_team = event_data["actions"][0]["selected_options"][0]["value"]
-	else:
-		return "There's been a error.", None
+		channel = event_data["channel"]["id"]
 		
 	#Search for author's team from author index
 	query = {
@@ -82,32 +86,90 @@ def set_team(event_data):
 	
 	es.index(index="author-index", doc_type='author', body=doc, refresh="true") 
 	
-	return "You have set yourself on the " + selected_team + " team!", None
+	slack_client.api_call("chat.delete", channel=channel, ts=event_data["message_ts"])
+	
+	slack_client.api_call("chat.postMessage", channel=channel, text="You now share notes with the " + selected_team + " team!", attachments=None)
+	return
 
-def review(event_data):
+def review_notes(event_data):
+	
+	#add replace_original to messages 
+	# add limit on number of returns
+	if event_data["type"] == 'interactive_message': 
+		print (event_data)
+		channel = event_data["channel"]["id"]
+		id = event_data["callback_id"].lstrip("note_selection")
+		es.delete(index="note-index", doc_type='note', id=id)
+		slack_client.api_call("chat.delete", channel=channel, ts=event_data["message_ts"])
+		message = "Note `" + event_data["original_message"]["attachments"][int(event_data["attachment_id"])-1]["text"] + "` has been deleted!"
+		slack_client.api_call("chat.postMessage", channel=channel, text=message, attachments=None)
+		return
+	
+	channel = event_data["event"]["channel"]	
+	query = {
+			"sort" : [
+				{ "timestamp": {"order": "desc"}} 
+			],
+			"query": {
+				"match": {
+					"author": event_data['event']['user'] 
+				}
+			}
+		}
+	
+	notes = es.search(index="note-index", body=query)
+	
+	if notes["hits"]["total"] == 0:
+		slack_client.api_call("chat.postMessage", channel=channel, text="You haven't saved any notes yet!")
+		return
+		
+	attachments = []
+	
+	for idx in range(notes["hits"]["total"]):
+		record = {}
+		print ("record " + str(idx))
+		record["text"] = notes["hits"]["hits"][idx]["_source"]["note"]
+		record["fallback"] = "Please upgrade slack to see this message!"
+		record["actions"] = [
+				{
+					"name": "option",
+					"text": "Delete",
+					"type": "button",
+					"value": "delete"
+				}   
+			]
+		record["callback_id"] = "note_selection" + notes["hits"]["hits"][idx]["_id"]
+		attachments.append(record)
+	
+	slack_client.api_call("chat.postMessage", channel=channel, text="Here are your notes:", attachments=attachments)
+	
 	print ("review")
 	
 def help(event_data):
 	print ("help")
-	return Path('help_menu.txt').read_text(), None 
+	channel = event_data["event"]["channel"]
+	help_menu = Path('help_menu.txt').read_text()
+	slack_client.api_call("chat.postMessage", channel=channel, text=help_menu, attachments=None)
+	return
 	
 def fallback(event_data):
-	print ("fallback")
-	return ":confounded: Sorry, I don't understand. Type 'help' to see a list of what I can do!", None
-
-def identify_command(event_data):
 	channel = event_data["event"]["channel"]
+	print ("fallback")
+	message = ":confounded: Sorry, I don't understand. Type 'help' to see a list of what I can do!"
+	slack_client.api_call("chat.postMessage", channel=channel, text=message, attachments=None)
+	return 
+
+def command_invoker(event_data):
+	patterns=['^save (.*)', '^find (.*)', '^team', '^help', '^review', '.*']
+	functions=['save_note', 'find_note', 'set_team', 'help', 'review_notes', 'fallback']
 	message_text = event_data["event"]['text']
-	patterns=['^save (.*)', '^find (.*)', '^team', '^help', '.*']
-	functions=['save_note', 'find_note', 'set_team', 'help', 'fallback']
-	
 	for idx, pattern in enumerate(patterns):
 		if re.compile(patterns[idx]).search(message_text) is not None:
 			return eval(functions[idx])(event_data)
 			
-def identify_callback(action_data):
-	callback_types=['team_selection']
-	functions=['set_team']
+def action_invoker(action_data):
+	callback_types=['team_selection', "note_selection"]
+	functions=['set_team', 'review_notes']
 	for idx, callback_type in enumerate(callback_types):
-		if action_data["callback_id"] == callback_types[idx]:
+		if action_data["callback_id"].startswith(callback_types[idx]):
 			return eval(functions[idx])(action_data)
