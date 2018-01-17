780e66e0b0e6c8d741cbf175d7e5fda72d2dec34
James Marcogliese <james.marcogliese@gmail.com>
2018-01-16 20:11:33 -0500

    Added message.updated to replace delete and new message

--- bin/commands.py
+++ bin/commands.py
@@ -50,6 +50,9 @@ def save_note(event_data):
 def find_note(event_data):
 	print ("findNote")
 	message = event_data['event']['text'].lstrip('find ')
+	
+	
+	
 
 def set_team(event_data):
 	print ("setTeam")
@@ -86,23 +89,20 @@ def set_team(event_data):
 	
 	es.index(index="author-index", doc_type='author', body=doc, refresh="true") 
 	
-	slack_client.api_call("chat.delete", channel=channel, ts=event_data["message_ts"])
-	
-	slack_client.api_call("chat.postMessage", channel=channel, text="You now share notes with the " + selected_team + " team!", attachments=None)
+	message = "You now share notes with the " + selected_team + " team!"
+	slack_client.api_call("chat.update", channel=channel, ts=event_data["message_ts"], text=message, attachments=None)
 	return
 
 def review_notes(event_data):
-	
-	#add replace_original to messages 
-	# add limit on number of returns
+
 	if event_data["type"] == 'interactive_message': 
 		print (event_data)
-		channel = event_data["channel"]["id"]
 		id = event_data["callback_id"].lstrip("note_selection")
 		es.delete(index="note-index", doc_type='note', id=id)
-		slack_client.api_call("chat.delete", channel=channel, ts=event_data["message_ts"])
+		
+		channel = event_data["channel"]["id"]
 		message = "Note `" + event_data["original_message"]["attachments"][int(event_data["attachment_id"])-1]["text"] + "` has been deleted!"
-		slack_client.api_call("chat.postMessage", channel=channel, text=message, attachments=None)
+		slack_client.api_call("chat.update", channel=channel, ts=event_data["message_ts"], text=message, attachments=None)
 		return
 	
 	channel = event_data["event"]["channel"]	
