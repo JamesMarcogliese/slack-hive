df27c7fd265eb4acfe46c9fd6867d1b16dd3e737
JamesMarcogliese <james.marcogliese@gmail.com>
2018-05-09 01:23:36 +0000

    Added message.app_home check

--- bin/app.py
+++ bin/app.py
@@ -21,12 +21,15 @@ globals.init()
 # Responder to direct messages
 @slack_events_adapter.on("message")
 def handle_message(event_data):
-	if ((event_data.get("event").get("subtype") is None) and (message_filter.is_direct_message(event_data))):
+	print ("\n -------- BEGINNING OF MESSAGE --------\n")
+	print (event_data)
+	print ("\n -------- END OF MESSAGE --------\n")
+	if ((event_data.get("event").get("channel_type") == "app_home") and (event_data.get("event").get("bot_id") is None)):
 		command_dispatcher.dispatch_event(event_data)
-		
+
 @slack_events_adapter.on("message_action")
 def message_action(action_data):
 	command_dispatcher.dispatch_action(action_data)
 
 # Flask server with the default endpoint on port 3000
-slack_events_adapter.start(port=3000)
+slack_events_adapter.start(host="0.0.0.0",port=3000)
